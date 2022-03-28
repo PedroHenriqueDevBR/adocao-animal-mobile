7839f521eaa8028f67e5fbb9d38baf3a57bd4d0b
pedrohenriquedevbr <p.lyma18@gmail.com>
2022-03-28 18:29:47 -0300

    chore: add assets to project

--- adocao_local/lib/src/shares/core/app_assets.dart
+++ adocao_local/lib/src/shares/core/app_assets.dart
@@ -1,3 +1,7 @@
 class AppAssets {
-  // String get defaultImage => 'assets/images/sunflowers.jpeg';
+  static String get logo => 'assets/images/paw-logo.png';
+  static String get cat => 'assets/images/adopt-cat.png';
+  static String get dog => 'assets/images/adopt-dog.png';
+  static String get avatar => 'assets/images/avatar.png';
+  static String get catAndDog => 'assets/images/cat-dog.png';
 }
