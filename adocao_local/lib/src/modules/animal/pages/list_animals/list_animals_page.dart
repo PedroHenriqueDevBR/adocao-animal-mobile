509d95e683dc82d248c25bda361aeac0339e40b3
pedrohenriquedevbr <p.lyma18@gmail.com>
2022-04-15 23:14:32 -0300

    refactor: apply desktop view

--- adocao_local/lib/src/modules/animal/pages/list_animals/list_animals_page.dart
+++ adocao_local/lib/src/modules/animal/pages/list_animals/list_animals_page.dart
@@ -1,5 +1,6 @@
 import 'package:adocao_local/src/shares/core/app_text_theme.dart';
 import 'package:flutter/material.dart';
+import 'dart:io';
 
 class ListAnimalsPage extends StatefulWidget {
   const ListAnimalsPage({Key? key}) : super(key: key);
@@ -41,10 +42,22 @@ class _ListAnimalsPageState extends State<ListAnimalsPage> {
             ),
           ),
           Expanded(
-            child: ListView.builder(
-              itemCount: 10,
-              itemBuilder: (context, index) => animalCard(),
-            ),
+            child: Platform.isAndroid || Platform.isIOS
+                ? ListView.builder(
+                    itemCount: 10,
+                    itemBuilder: (context, index) => animalCard(),
+                  )
+                : GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                    ),
+                    addAutomaticKeepAlives: true,
+                    addRepaintBoundaries: true,
+                    itemCount: 10,
+                    semanticChildCount: 2,
+                    itemBuilder: (context, index) => animalgridCard(),
+                  ),
           ),
         ],
       ),
@@ -54,6 +67,71 @@ class _ListAnimalsPageState extends State<ListAnimalsPage> {
       ),
     );
   }
+
+  Widget animalgridCard() => Card(
+        margin: const EdgeInsets.all(8.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    width: 200,
+                    height: double.maxFinite,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(4.0),
+                        bottomLeft: Radius.circular(4.0),
+                      ),
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                            'https://images.pexels.com/photos/1904105/pexels-photo-1904105.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Rex',
+                          style: _textStyle.titleStyle,
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.more_vert),
+                        ),
+                      ],
+                    ),
+                    const Divider(),
+                    Flexible(
+                      child: RichText(
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 10,
+                        strutStyle: const StrutStyle(fontSize: 12.0),
+                        text: TextSpan(
+                            style: _textStyle.descriptionStyle,
+                            text:
+                                'Mussum Ipsum, cacilds vidis litro abertis. Mé faiz elementum girarzis, nisi eros vermeio.Quem num gosta di mé, boa gentis num é.Sapien in monti palavris qui num significa nadis i pareci latim.Delegadis gente finis, bibendum egestas augue arcu ut est. Mussum Ipsum, cacilds vidis litro abertis. Mé faiz elementum girarzis, nisi eros vermeio.Quem num gosta di mé, boa gentis num é.Sapien in monti palavris qui num significa nadis i pareci latim.Delegadis gente finis, bibendum egestas augue arcu ut est.'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
 
   Widget animalCard() => Card(
         margin: const EdgeInsets.all(8.0),
