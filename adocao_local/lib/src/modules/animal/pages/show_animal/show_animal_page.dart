c91fabd050d5a7ab85ba79833f7cd33082287dc6
pedrohenriquedevbr <p.lyma18@gmail.com>
2022-04-27 23:53:31 -0300

    style: Create presentation page style

--- adocao_local/lib/src/modules/animal/pages/show_animal/show_animal_page.dart
+++ adocao_local/lib/src/modules/animal/pages/show_animal/show_animal_page.dart
@@ -1,5 +1,7 @@
 import 'package:adocao_local/src/shares/core/app_text_theme.dart';
 import 'package:flutter/material.dart';
+import 'dart:io' show Platform;
+import 'package:asuka/asuka.dart' as asuka;
 
 class ShowAnimalPage extends StatefulWidget {
   const ShowAnimalPage({Key? key}) : super(key: key);
@@ -10,23 +12,163 @@ class ShowAnimalPage extends StatefulWidget {
 
 class _ShowAnimalPageState extends State<ShowAnimalPage> {
   final _textStyle = AppTextStyle();
+
+  void showBottomPage() {
+    asuka.showModalBottomSheet(
+      builder: (context) => Column(
+        children: [
+          AppBar(
+            centerTitle: false,
+            automaticallyImplyLeading: false,
+            title: const Text('Solicitações'),
+            backgroundColor: Theme.of(context).colorScheme.secondary,
+          ),
+          Expanded(
+            child: ListView.separated(
+              itemCount: 10,
+              separatorBuilder: (_, __) => const Divider(),
+              itemBuilder: (ctx, index) => ListTile(
+                title: const Text('Nome do solicitante'),
+                subtitle: const Text('(86) 91234-5678'),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      color: Colors.green,
+                      onPressed: () {},
+                      icon: const Icon(Icons.check_circle),
+                    ),
+                    IconButton(
+                      color: Colors.red,
+                      onPressed: () {},
+                      icon: const Icon(Icons.cancel),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Adoção App'),
       ),
-      body: SingleChildScrollView(
+      body: Platform.isAndroid || Platform.isIOS
+          ? childrenWidgets()
+          : desktopChildrenWidgets(size),
+    );
+  }
+
+  Widget desktopChildrenWidgets(Size size) => Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(
+              height: size.height,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                      'https://images.pexels.com/photos/1904105/pexels-photo-1904105.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: SizedBox(
+              height: size.height,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Rex',
+                            style: _textStyle.titleStyle,
+                          ),
+                          ElevatedButton(
+                            onPressed: showBottomPage,
+                            style: ElevatedButton.styleFrom(
+                              primary: Theme.of(context).colorScheme.secondary,
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 12.0, horizontal: 4.0),
+                            ),
+                            child: Row(
+                              children: [
+                                Container(
+                                  padding: const EdgeInsets.all(4.0),
+                                  margin: const EdgeInsets.only(right: 4.0),
+                                  decoration: BoxDecoration(
+                                    color: Theme.of(context).primaryColor,
+                                    borderRadius: const BorderRadius.all(
+                                      Radius.circular(4.0),
+                                    ),
+                                  ),
+                                  child: Text(
+                                    '5',
+                                    style: _textStyle.textButton,
+                                  ),
+                                ),
+                                const Text('Solicitações'),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Text(
+                        'Cachorro viralata muito bem cuidado e buscando um novo lar.',
+                        textAlign: TextAlign.center,
+                        style: _textStyle.descriptionStyle,
+                      ),
+                    ),
+                    const Divider(),
+                    Text(
+                      'Caderno de vacinação',
+                      style: _textStyle.textButton,
+                    ),
+                    ListView.separated(
+                      itemCount: 5,
+                      itemBuilder: (ctx, index) => ListTile(
+                        leading: const Icon(Icons.vaccines),
+                        title: Text('data ${index + 1}'),
+                        subtitle: const Text('00/00/0000'),
+                      ),
+                      separatorBuilder: (_, __) => const Divider(),
+                      physics: const NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      scrollDirection: Axis.vertical,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      );
+
+  Widget childrenWidgets() => SingleChildScrollView(
         child: Column(
           children: [
             Container(
               width: double.infinity,
               height: 250,
               decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(4.0),
-                  bottomLeft: Radius.circular(4.0),
-                ),
                 image: DecorationImage(
                   fit: BoxFit.cover,
                   image: NetworkImage(
@@ -44,7 +186,7 @@ class _ShowAnimalPageState extends State<ShowAnimalPage> {
                     style: _textStyle.titleStyle,
                   ),
                   ElevatedButton(
-                    onPressed: () {},
+                    onPressed: showBottomPage,
                     style: ElevatedButton.styleFrom(
                       primary: Theme.of(context).colorScheme.secondary,
                       padding: const EdgeInsets.symmetric(
@@ -82,9 +224,24 @@ class _ShowAnimalPageState extends State<ShowAnimalPage> {
               ),
             ),
             const Divider(),
+            Text(
+              'Caderno de vacinação',
+              style: _textStyle.textButton,
+            ),
+            ListView.separated(
+              itemCount: 5,
+              itemBuilder: (ctx, index) => ListTile(
+                leading: const Icon(Icons.vaccines),
+                title: Text('data ${index + 1}'),
+                subtitle: const Text('00/00/0000'),
+              ),
+              separatorBuilder: (_, __) => const Divider(),
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              scrollDirection: Axis.vertical,
+            ),
+            const SizedBox(height: 100.0),
           ],
         ),
-      ),
-    );
-  }
+      );
 }
