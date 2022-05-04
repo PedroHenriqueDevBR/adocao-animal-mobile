e09409985f480515345a957768bf1c8c4541766c
pedrohenriquedevbr <p.lyma18@gmail.com>
2022-05-04 00:25:18 -0300

    feat: animal class serialization

--- adocao_local/lib/src/modules/animal/models/animal_model.dart
+++ adocao_local/lib/src/modules/animal/models/animal_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'adoption_request_model.dart';
 import 'animal_photo_model.dart';
 import 'animal_type_model.dart';
@@ -32,4 +34,45 @@ class AnimalModel {
   }
 
   get formattedCreateAt => '${createAt.day}/${createAt.month}/${createAt.year}';
+
+  Map<String, dynamic> toMap() {
+    final result = <String, dynamic>{};
+
+    result.addAll({'name': name});
+    result.addAll({'breed': breed});
+    result.addAll({'age': age});
+    result.addAll({'sex': sex});
+    result.addAll({'type': animalType.id});
+
+    return result;
+  }
+
+  factory AnimalModel.fromMap(Map<String, dynamic> map) {
+    final animalModel = AnimalModel(
+      id: map['id'] ?? null,
+      name: map['name'] ?? '',
+      breed: map['breed'] ?? '',
+      age: map['age']?.toInt() ?? 0,
+      sex: map['sex'] ?? '',
+      adopted: map['adopted'] ?? false,
+      blocked: map['blocked'] ?? false,
+      createAt: DateTime.parse(map['create_at']),
+      animalType: AnimalTypeModel.fromMap(map['type']),
+    );
+    animalModel.photos = List<AnimalPhotoModel>.from(
+      map['all_photos']?.map((x) => AnimalPhotoModel.fromMap(x)),
+    );
+    animalModel.vaccines = List<VaccineModel>.from(
+      map['all_vaccines']?.map((x) => VaccineModel.fromMap(x)),
+    );
+    return animalModel;
+  }
+
+  static List<AnimalModel> fromMapList(List<dynamic> list) {
+    List<AnimalModel> result = [];
+    for (Map<String, dynamic> map in list) {
+      result.add(AnimalModel.fromMap(map));
+    }
+    return result;
+  }
 }
