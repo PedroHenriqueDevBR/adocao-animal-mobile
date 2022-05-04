823bd40dd7be6b49eb29416fb11f92a5dd6ecf1f
pedrohenriquedevbr <p.lyma18@gmail.com>
2022-05-03 23:49:19 -0300

    test: implement vaccine repository tests

--- adocao_local/test/src/modules/animal/repositories/animal_vaccine_repository_test.dart
+++ adocao_local/test/src/modules/animal/repositories/animal_vaccine_repository_test.dart
@@ -1,5 +1,7 @@
+import 'package:adocao_local/src/modules/animal/models/animal_model.dart';
 import 'package:adocao_local/src/modules/animal/models/animal_type_model.dart';
-import 'package:adocao_local/src/modules/animal/repositories/animal_type_repository.dart';
+import 'package:adocao_local/src/modules/animal/models/vaccine_book_model.dart';
+import 'package:adocao_local/src/modules/animal/repositories/animal_vaccine_repository.dart';
 import 'package:adocao_local/src/shares/interfaces/app_data_interface.dart';
 import 'package:adocao_local/src/shares/interfaces/client_http_interface.dart';
 import 'package:adocao_local/src/shares/models/http_response_model.dart';
@@ -14,8 +16,62 @@ void main() {
   final _client = HttpMock();
   final _appData = JwtMock();
 
-  // AnimalTypeRepository _repository = AnimalTypeRepository(
-  //   client: _client,
-  //   appData: _appData,
-  // );
+  AnimalVaccineRepository _repository = AnimalVaccineRepository(
+    client: _client,
+    appData: _appData,
+  );
+
+  test('Deve retornar um objeto do tipo vacina', () async {
+    when(() => _client.post(any(), any(), jwtKey: any(named: 'jwtKey')))
+        .thenAnswer(
+      (_) async => HttpResponseModel(
+        statusCode: 200,
+        data: addedVaccineResponse,
+        headers: {},
+      ),
+    );
+    when(() => _appData.getJWT()).thenAnswer(
+      (_) async => 'some_key',
+    );
+
+    AnimalModel animal = AnimalModel(
+      name: '',
+      breed: '',
+      age: 1,
+      sex: '',
+      animalType: AnimalTypeModel(name: 'name'),
+    );
+    VaccineModel vaccine = VaccineModel(name: 'name');
+    VaccineModel response = await _repository.addVaccine(
+      vaccine,
+      animal,
+    );
+    expect(response.id, equals(4));
+    expect(response.name, equals('Vacina teste'));
+    expect(response.date!.day, equals(17));
+    expect(response.date!.month, equals(4));
+    expect(response.date!.year, equals(2022));
+  });
+
+  test('Deve remover a vacina e retornar void', () async {
+    when(() => _client.delete(any(), jwtKey: any(named: 'jwtKey'))).thenAnswer(
+      (_) async => HttpResponseModel(
+        statusCode: 200,
+        headers: {},
+      ),
+    );
+    when(() => _appData.getJWT()).thenAnswer(
+      (_) async => 'some_key',
+    );
+
+    VaccineModel vaccine = VaccineModel(name: 'name');
+    await _repository.removeVaccine(vaccine);
+  });
 }
+
+Map<String, dynamic> addedVaccineResponse = {
+  "id": 4,
+  "vaccine_name": "Vacina teste",
+  "date": "2022-04-17",
+  "animal": 1
+};
